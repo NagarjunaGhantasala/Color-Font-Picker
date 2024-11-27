c833664a9ef1207cec33fec9786b0680b495d37f
Nagarjuna Ghantasala <arjunghantasala16@gmail.com>
2024-11-27 12:36:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+function textcolorred(){
+    let r1 = document.getElementById('text')
+    r1.style.color='red'
+}
+function bgcolorred(){
+    let r2 = document.getElementById('text')
+    r2.style.backgroundColor='red'
+}
+function textcolorgreen(){
+    let g1 = document.getElementById('text')
+    g1.style.color='green'
+}
+function bgcolorgreen(){
+    let g2 = document.getElementById('text')
+    g2.style.backgroundColor='green'
+}
+function textcolororange(){
+    let o1 = document.getElementById('text')
+    o1.style.color='orange'
+}
+function bgcolororange(){
+    let o2 = document.getElementById('text')
+    o2.style.backgroundColor='orange'
+}
+function textcoloryellow(){
+    let y1 = document.getElementById('text')
+    y1.style.color='yellow'
+}
+function bgcoloryellow(){
+    let y2 = document.getElementById('text')
+    y2.style.backgroundColor='yellow'
+}
+function textcolorblue(){
+    let b1 = document.getElementById('text')
+    b1.style.color='blue'
+}
+function bgcolorblue(){
+    let b2 = document.getElementById('text')
+    b2.style.backgroundColor='blue'
+}
+function textcolorbrown(){
+    let br1 = document.getElementById('text')
+    br1.style.color='brown'
+}
+function bgcolorbrown(){
+    let br2 = document.getElementById('text')
+    br2.style.backgroundColor='brown'
+}
+function textcolorblack(){
+    let br1 = document.getElementById('text')
+    br1.style.color='black'
+}
+function bgcolorblack(){
+    let br2 = document.getElementById('text')
+    br2.style.backgroundColor='black'
+}
+function textcolorwhite(){
+    let br1 = document.getElementById('text')
+    br1.style.color='white'
+}
+function bgcolorwhite(){
+    let br2 = document.getElementById('text')
+    br2.style.backgroundColor='white'
+}
+function textcolorpink(){
+    let br1 = document.getElementById('text')
+    br1.style.color='pink'
+}
+function bgcolorpink(){
+    let br2 = document.getElementById('text')
+    br2.style.backgroundColor='pink'
+}
+function textcoloraqua(){
+    let br1 = document.getElementById('text')
+    br1.style.color='aqua'
+}
+function bgcoloraqua(){
+    let br2 = document.getElementById('text')
+    br2.style.backgroundColor='aqua'
+}
+
+function font1()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="Times New Roman"
+    }
+function font2()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="Roboto"
+    }
+function font3()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="Courier New"
+    }
+function font4()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="cursive"
+    }
+function font5()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="Verdana"
+    }
+function font6()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="Impact"
+    }
+function font7()
+    {
+        var a = document.getElementById('text') 
+        a.style.fontFamily="Arial"
+    }
+function font8()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="Georgia"
+    }
+function font9()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="serif";
+    }
+function font10()
+    {
+        var a = document.getElementById('text')
+        a.style.fontFamily="sans-serif";
+    }
